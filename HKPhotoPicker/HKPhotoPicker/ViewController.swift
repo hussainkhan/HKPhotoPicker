9e7c3f01b89cb8db6d3cce2a4805943fb7831ebe
Hussain Khan <wcities@Frasers-iMac.local>
2017-01-10 11:32:17 +0530

    First commit of HKPhotoPicker
    Can choose photo from device library.
    Can Pic photo from camera.

---
+++ HKPhotoPicker/HKPhotoPicker/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  HKPhotoPicker
+//
+//  Created by wcities on 09/01/17.
+//  Copyright © 2017 wcities. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+    }
+
+
+    @IBAction func takePhoto(_ sender: UIButton) {
+        
+        imagePicker.sourceType = .camera
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+
+    @IBAction func selectPhoto(_ sender: UIButton) {
+        
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)        
+    }
+}
+
+
+extension ViewController:UIImagePickerControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage{
+           self.imageView.image    = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController:UINavigationControllerDelegate{
+    
+}
+
